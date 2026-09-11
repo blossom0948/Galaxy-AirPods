109f73ad3cc1ceb731104d5d54d1d1b4d6e11e96
blossom0948 <blossom0948@gmail.com>
2026-09-11 22:48:12 +0900

    Prepare v0.4.11 popup motion update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.galaxyairpods"
         minSdk = 29
         targetSdk = 35
-        versionCode = 32
-        versionName = "0.4.10"
+        versionCode = 33
+        versionName = "0.4.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
